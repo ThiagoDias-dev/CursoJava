67235b68a4fbfa25c4dbbcfc4ad95afcb6319f2d
Thiago Dias <dev.thiagodias@gmail.com>
2024-06-27 16:26:20 -0300

    Fndamentos básicos

---
+++ src/Fundamentos/fundamentosBasicos.java
@@ -0,0 +1,19 @@
+package Fundamentos;
+
+public class fundamentosBasicos {
+    public static void main(String[] args) {
+        //Declarações de valores numéricas
+        int inteiro = 0; //declara números inteiros
+        float reais = 1_445.57f; //declara numeros reais
+        double decimais = 4.5; //declara números flutuantes
+        
+        //Declarações de valores booleanos 
+        boolean férias = true; //Declara verdadeiro ou falso
+
+        //Declarações de caracteres 
+        char sexo = 'M'; //Declara caracteres
+        String frase = "Bom Dia"; //Declara frase
+        final String texto = "Inauterado"; //Declara uma constante, inauterada
+    }
+        
+}
---
+++ src/Fundamentos/manipulacaoDeTexto.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+public class manipulacaoDeTexto {
+    public static void main(String[] args) {
+        
+        String texto = "Bom dia X";//Texto a ser manipulado
+
+        
+
+    }
+
+}
