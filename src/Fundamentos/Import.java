fa47621c187b12ee9271d0f17a5d487fd9949850
Thiago Dias <dev.thiagodias@gmail.com>
2024-06-27 17:51:57 -0300

    Atualização das aulas de Java

---
+++ src/Fundamentos/Import.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+        
+        Date s = new Date();
+        System.out.println(s);
+    }
+
+}
--- src/Fundamentos/manipulacaoDeTexto.java
+++ src/Fundamentos/manipulacaoDeTexto.java
@@ -3,9 +3,22 @@ package Fundamentos;
 public class manipulacaoDeTexto {
     public static void main(String[] args) {
         
-        String texto = "Bom dia X";//Texto a ser manipulado
+        String s = "Bom dia X";//Texto a ser manipulado
+            s= s.replace("X", "Senhora");//substitui um caracter por outro
+            s= s.toUpperCase();//coloca as letras maiúsculas
+            s= s.concat("!!!");//Concatena algo ao final da string
+        System.out.println(s);
 
-        
+        String x = "Léo".toUpperCase();
+        System.out.println(x);
+
+        //OUTRO MODO DE ALTERAR O TEXTO
+        String y = "Bom dia X"//Texto a ser manipulado
+                .replace("X", "Gui")            
+                .toUpperCase()
+                .concat("!!!");
+
+
 
     }
 
